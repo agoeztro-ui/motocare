24d72d1d0ced2a84fa7cb133c09d38a2d1c41cff
agoeztro-ui <agoeztro@gmail.com>
2026-09-21 22:52:21 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+const C='mc-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
